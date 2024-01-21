9f77b90b670bef4dd1d6466056f0d77fad3a338b
Ignacio Rviera <riveign@gmail.com>
2024-01-21 20:00:24 +0100

    added mark done turbo stream

--- app/controllers/daily_habits_controller.rb
+++ app/controllers/daily_habits_controller.rb
@@ -5,10 +5,8 @@ class DailyHabitsController < ApplicationController
     if @daily_habit.update(done: true)
       @daily_habits = find_daily_habits_for_user
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace('daily_habits_list', partial: 'daily_habits/daily_habits_list',
-                                                                         locals: { daily_habits: @daily_habits })
-        end
+        format.turbo_stream
+        format.html { redirect_to root_path }
       end
     else
       redirect_to root_path, alert: 'Unable to update the habit.'
