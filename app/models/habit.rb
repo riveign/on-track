857476c781cd89a0ea732ae0ddc2584a82e1fb82
Ignacio Rivera Axmann <riveign@gmail.com>
2024-02-07 01:20:37 +0100

    checkpoint

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -27,7 +27,7 @@ class Habit < ApplicationRecord
   end
 
   def create_daily_habits_for_user
-    daily_habits.create!(user:, done: false) if active_days.include?(Date.current.wday)
+    daily_habits.create!(user:, done: false)
   end
 
   def set_disabled_to_false
