8dec38d80c655be106bd39b1cd63f153c953149f
Ignacio Rivera Axmann <riveign@gmail.com>
2024-02-11 16:36:24 +0100

    new tabs layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
     return unless user_signed_in?
 
     DailyHabitService.new(current_user).create_daily_habits
-    @habits = Habit.where(user: current_user)
     @daily_habits = current_user.daily_habits_for_today
     @reminders = current_user.reminders.upcoming
   end
