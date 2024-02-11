19a932cb6e44e9b2257be48337b2b5d845062ee5
Ignacio Rivera Axmann <riveign@gmail.com>
2024-02-11 18:58:43 +0100

    telgram daily reminder

---
+++ app/jobs/daily_habit_reminder_job.rb
@@ -0,0 +1,14 @@
+class DailyHabitReminderJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    User.all.each do |user|
+      next unless user.telegram_id && user.daily_habits.not_done.any?
+
+      user.daily_habits.not_done.sample(1).each do |daily_habit|
+        Telegram.bot.send_message(chat_id: user.telegram_id,
+                                  text: "Hoy no te olvides de #{daily_habit.habit.name}!")
+      end
+    end
+  end
+end
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,3 +5,7 @@ scheduler = Rufus::Scheduler.new
 scheduler.cron '0 3 * * *' do
   DailyHabitCreationJob.perform_later
 end
+
+scheduler.cron '0 12,18 * * *' do
+  DailyHabitReminderJob.perform_later
+end
